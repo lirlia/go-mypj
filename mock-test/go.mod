4f56b14164a5c91ed2c96820dc9f7217fc019908
gin <lirlia5000@gmail.com>
2021-08-18 00:49:24 +0900

    add mock

--- mock-test/go.mod
+++ mock-test/go.mod
@@ -2,4 +2,4 @@ module github.com/lirlia/go-mypj/mock-test
 
 go 1.16
 
-require github.com/golang/mock v1.6.0 // indirect
+require github.com/golang/mock v1.6.0
--- mock-test/main.go
+++ mock-test/main.go
@@ -4,9 +4,17 @@ type ApiClient interface {
 	Request(string) (string, error)
 }
 
+type RegisterInterface interface {
+	Register(string) (string, error)
+}
+
+// データを登録する
 type DataRegister struct {
 	client ApiClient
 }
+
+var _ ApiClient = &DataRegister{}
+var _ RegisterInterface = &DataRegister{}
 
 func (d *DataRegister) Register(data string) (string, error) {
 	result, err := d.client.Request(data)
@@ -14,4 +22,11 @@ func (d *DataRegister) Register(data string) (string, error) {
 		return "", err
 	}
 	return result, nil
+}
+
+func main() {
+
+	d := &DataRegister{}
+	a, err := d.client.Request("GET")
+	v, err := d.Request("GET")
 }
