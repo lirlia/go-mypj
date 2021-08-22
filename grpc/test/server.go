74244521592cf47430599eab0bacae44e17a22f5
gin <lirlia5000@gmail.com>
2021-08-22 20:44:44 +0900

    add grpc

---
+++ grpc/test/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/lirlia/go-mypj/grpc/myproto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedCloudServer
+}
+
+func (s *server) GetData(ctx context.Context, in *pb.DateRequest) (*pb.DateResponse, error) {
+	locale := in.getLocale()
+	log.Printf("Received: %v", locale)
+	now := time.Now()
+	nowUTC := now.UTC()
+
+	switch locale {
+	case "jst":
+		jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+		return &pb.DateResponse{date: nowUTC.In(jst)}, nil
+	default:
+		return &pb.DateResponse{date: nowUTC}, nil
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
