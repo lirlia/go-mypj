b8c6ea986913008afdfa8bea9fe6eb17156f2ffb
gin <lirlia5000@gmail.com>
2021-08-28 23:32:58 +0900

    add

--- func-test/main.go
+++ func-test/main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"runtime"
-)
+import "fmt"
 
 type TestFunc func(int, int) int
 type ValuePool struct {
@@ -24,25 +20,7 @@ func (t ValuePool) Run(f TestFunc) {
 	fmt.Println(f(t.a, t.b))
 }
 
-func preStart() {
-	_, f, _, _ := runtime.Caller(0)
-	bytes, _ := ioutil.ReadFile(f)
-	fmt.Println("## source")
-	fmt.Println()
-	fmt.Println("```go")
-	fmt.Println(string(bytes))
-	fmt.Println("```")
-	fmt.Println()
-	fmt.Println("## Result")
-	fmt.Println()
-	fmt.Println("```sh")
-}
-func preEnd() { fmt.Println("```") }
-
 func main() {
-
-	preStart()
-	defer preEnd()
 	a, b := 100, 100
 
 	var t TestFunc
