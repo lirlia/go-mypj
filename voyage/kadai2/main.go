48bf16c667735b93b79bb3fd2cf3852f659505e5
gin <10017674+lirlia@users.noreply.github.com>
2021-09-11 17:43:23 +0900

    A .gitignore M main.go M model/model.go D tmp/build-errors.log D tmp/main

--- voyage/kadai2/main.go
+++ voyage/kadai2/main.go
@@ -1,61 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"sync"
 
 	"github.com/lirlia/go-mypj/voyage/kadai2/model"
-	"golang.org/x/net/html"
 )
 
-func Get(url string) (*model.Page, error) {
-	page := new(model.Page)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var f func(*html.Node)
-	// fはDOMツリーを再帰的にトラバースするための手続きです。
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "title" {
-			page.Title = n.FirstChild.Data
-		}
-
-		// 再帰的にノードをおっていくために、次のノードを探し、
-		// ノードが存在すれば再びこのfを実行する、ということをしています。
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-
-	return page, nil
-}
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		querys, _ := url.ParseQuery(r.URL.RawQuery)
+
 		if querys.Get("url") != "" {
-			p, err := Get(querys.Get("url"))
-			if err != nil && err != io.EOF {
-				panic(err)
+			urls := strings.Split(querys.Get("url"), ",")
+			c := make(chan model.Page, len(urls))
+			wg := &sync.WaitGroup{}
+
+			for _, u := range urls {
+				url := u
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					p, err := model.Get(url)
+					if err != nil && err != io.EOF {
+						panic(err)
+					}
+					c <- *p
+				}()
 			}
 
-			msg := fmt.Sprintf("%v", p.Title)
-			w.Write([]byte(msg))
+			wg.Wait()
 
+			// 終了までまつ
+			var Pages []model.Page
+
+			for {
+				Pages = append(Pages, <-c)
+				if len(c) == 0 {
+					close(c)
+					break
+				}
+			}
+
+			enc := json.NewEncoder(w)
+			enc.SetIndent("", "    ")
+			enc.Encode(Pages)
 		}
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
--- voyage/kadai2/model/model.go
+++ voyage/kadai2/model/model.go
@@ -1,5 +1,100 @@
 package model
 
+import (
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
 type Page struct {
-	Title string
+	URL         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	OgTitle     string `json:"og_title"`
+	OgImage     string `json:"og_image"`
+}
+
+func isDescription(attrs []html.Attribute) bool {
+	for _, attr := range attrs {
+		if attr.Key == "name" && attr.Val == "description" {
+			/* KeyがnameでValがdescriptionか？ */
+			return true
+		}
+	}
+	return false
+}
+
+func isOgTitle(attrs []html.Attribute) bool {
+	for _, attr := range attrs {
+		if attr.Key == "property" && attr.Val == "og:title" {
+			/* KeyがnameでValがdescriptionか？ */
+			return true
+		}
+	}
+	return false
+}
+
+func isOgImage(attrs []html.Attribute) bool {
+	for _, attr := range attrs {
+		if attr.Key == "property" && attr.Val == "og:image" {
+			/* KeyがnameでValがdescriptionか？ */
+			return true
+		}
+	}
+	return false
+}
+
+func Get(url string) (*Page, error) {
+	page := &Page{}
+	page.URL = url
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var f func(*html.Node)
+	// fはDOMツリーを再帰的にトラバースするための手続きです。
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" {
+			page.Title = n.FirstChild.Data
+		}
+
+		if n.Type == html.ElementNode && n.Data == "meta" {
+			if isDescription(n.Attr) {
+				for _, attr := range n.Attr {
+					// contentの中身を取得
+					page.Description = attr.Val
+				}
+			}
+
+			if isOgTitle(n.Attr) {
+				for _, attr := range n.Attr {
+					// contentの中身を取得
+					page.OgTitle = attr.Val
+				}
+			}
+
+			if isOgImage(n.Attr) {
+				for _, attr := range n.Attr {
+					// contentの中身を取得
+					page.OgImage = attr.Val
+				}
+			}
+		}
+
+		// 再帰的にノードをおっていくために、次のノードを探し、
+		// ノードが存在すれば再びこのfを実行する、ということをしています。
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+	return page, nil
 }
